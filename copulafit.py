b21354e938982ceb50b07906652112da10a3b24a
Kiran Karra <kkarranc@vt.edu>
2015-06-01 13:40:39 -0400

    added pkte estimators for 4 different copula types

--- copulafit.py
+++ copulafit.py
@@ -23,6 +23,8 @@ import math
 import numpy as np
 
 import multivariate_stats
+from invcopulastat import invcopulastat
+from scipy.stats import kendalltau
 
 """
 copulafit.py contains routines which provide use various techniques, as
@@ -47,10 +49,11 @@ def copulafit(family, X, algorithm):
            a numpy array of shape = M x N, where M is the number of samples 
            and N is the dimensionality of the data
       algorithm -- must be one of the following strings:
-        'IFM' - Inference Functions for Margin's method
-        'CML' - Canonical Maximum Likelihood
-        'Dependency' - Use's Kendall's Tau or Spearman's Rho relationships to the 
-                       copula family's dependency parameter 
+        'MLE'  - Maximum Likelihood method
+        'AMLE' - Approximate Maximum Likelihood method
+        'PKTE' - Use's Pairwise Kendall's Tau estimator's relationship to the 
+                       copula family's dependency parameter (only applicalble to
+                       Clayton, Gumbel, or Frank copula's currently)
                        
     Outputs:
       the dependency parameter for the copula
@@ -58,35 +61,64 @@ def copulafit(family, X, algorithm):
     """
     algorithm_lc = algorithm.lower()
     family_lc    = family.lower()
-    if(algorithm_lc=='ifm'):
-        raise Exception('IFM method not yet supported!')
-    elif(algorithm_lc=='cml'):
-        raise Exception('CML method not yet supported!')
-    elif(algorithm_lc=='dependency'):
+    dep_param    = None
+    if(algorithm_lc=='MLE'):
+        raise ValueError('MLE method not yet supported!')
+    elif(algorithm_lc=='AMLE'):
+        raise ValueError('Approximate MLE method not yet supported!')
+    elif(algorithm_lc=='PKTE'):
         if(family_lc=='gaussian'):
-            _gauss_dependency(X)
+            dep_param = _gaussian_PKTE(X)
         elif(family_lc=='t'):
-            _t_dependency(X)
+            dep_param = _t_PKTE(X)
         elif(family_lc=='clayton'):
-            _clayton_dependency(X)
+            dep_param = _clayton_PKTE(X)
         elif(family_lc=='gumbel'):
-            _gumbel_dependency(X)
+            dep_param = _gumbel_PKTE(X)
         elif(family_lc=='frank'):
-            _frank_dependency(X)
+            dep_param = _frank_PKTE(X)
     else:
-        raise Exception('Unsupported Algorithm!')
+        raise ValueError('Unsupported Algorithm or options!')
     
-def _gauss_dependency(X):
-    pass
+    return dep_param
     
-def _t_dependency(X):
-    pass
+def _guassian_PKTE(X):
+    # the algorithm for this comes from the paper:
+    # "Gaussian Copula Precision Estimation with Missing Values" 
+    # by Huahua Wang, Faridel Fazayeli, Soumyadeep Chatterjee, Arindam Banerjee
+    N = X.shape[1]
+    sigma_hat = np.ones((N,N))
+    for dim1 in range(0,N-1):
+        for dim2 in range(dim1+1,N):
+            rho = np.sin(math.pi/2 * kendalltau(X[:,dim1],X[:,dim2]))
+            sigma_hat[dim1][dim2] = rho
+            sigma_hat[dim2][dim1] = rho
 
-def _clayton_dependency(X):
-    pass
+# TODO: T copula stuff
+def _t_PKTE(X):
+    return None
+    
+def _clayton_PKTE(X):
+    # calculate empirical kendall's tau
+    ktau = multivariate_stats.kendalls_tau(X)
+    # inverse to find dependency parameter
+    alpha = invcopulastat('Clayton', 'kendall', ktau)
+    
+    return alpha
 
-def _gumbel_dependency(X):
-    pass
+def _gumbel_PKTE(X):
+    # calculate empirical kendall's tau
+    ktau = multivariate_stats.kendalls_tau(X)
+    # inverse to find dependency parameter
+    alpha = invcopulastat('Gumbel', 'kendall', ktau)
+    
+    return alpha
 
-def _frank_dependency(X):
-    pass
+
+def _frank_PKTE(X):
+    # calculate empirical kendall's tau
+    ktau = multivariate_stats.kendalls_tau(X)
+    # inverse to find dependency parameter
+    alpha = invcopulastat('Frank', 'kendall', ktau)
+    
+    return alpha
