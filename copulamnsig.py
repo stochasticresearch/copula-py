fixed empirical multinomial signature calculation

copulamnsig.py
@@ -25,9 +25,6 @@ import numpy as np
 from cvolume import cvolume
 import multivariate_stats
 
-from scipy.stats import norm
-from scipy.stats import expon
-
 from ecdf import probability_integral_transform
 
 def copulamnsig(family, tau, K):
@@ -63,8 +60,9 @@ def copulamnsig(family, tau, K):
     # left coordinates of the box and the upper right coordinates of the box
     # are stored as keys 'u1v1' and 'u2v2', and then the actual value of the 
     # multinomial signature in that grid is stored as 'val'
+    #mnsig = []
+    
     mnsig = []
-            
     for coord in coords_list:
         # compute the C-volume and store
         u1v1 = coord[0]
@@ -72,13 +70,14 @@ def copulamnsig(family, tau, K):
         u2v1 = coord[2]
         u2v2 = coord[3]
         val = cvolume(family, u1v1, u1v2, u2v1, u2v2, 'kendall', tau)
-        tmp = {}
-        tmp['u1v1'] = u1v1
-        tmp['u1v2'] = u1v2
-        tmp['u2v1'] = u2v1
-        tmp['u2v2'] = u2v2
-        tmp['val']  = val
-        mnsig.append(tmp)
+        #tmp = {}
+        #tmp['u1v1'] = u1v1
+        #tmp['u1v2'] = u1v2
+        #tmp['u2v1'] = u2v1
+        #tmp['u2v2'] = u2v2
+        #tmp['val']  = val
+        #mnsig.append(tmp)
+        mnsig.append(val[0])
     
     return mnsig
 
@@ -127,8 +126,7 @@ def empirical_copulamnsig(X, K):
             # RV 2 that we are comparing
             tmp['rv2'] = dim2+1
             # the value for the zone -- initialize to 0
-            for ii in range(1,K*K+1):
-                tmp[str(ii)] = 0.0
+            esig_vec = np.zeros(K*K)
             
             # there is probably a more efficient way to do this than to loop
             # over each value, but this is a first cut at implementing this
@@ -138,17 +136,20 @@ def empirical_copulamnsig(X, K):
             for ii in range(0,M):
                 # find which zone this specific (u,v) sample falls in
                 for jj in range(0,K*K):
-                    u1 = coords_list[jj][0][0][0][0]
-                    v1 = coords_list[jj][0][0][0][1]
-                    u2 = coords_list[jj][0][3][0][0]
-                    v2 = coords_list[jj][0][3][0][1]
+                    u1 = coords_list[jj][0][0][0]
+                    v1 = coords_list[jj][0][0][1]
+                    u2 = coords_list[jj][3][0][0]
+                    v2 = coords_list[jj][3][0][1]
                     
-                    if(u(ii) >= u1 and u(ii) < u2 and 
-                       v(ii) >= v1 and v(ii) < v2):
+                    if(u[ii] >= u1 and u[ii] < u2 and 
+                       v[ii] >= v1 and v[ii] < v2):
                         # add one to the zone that it falls into
-                        tmp[str(jj)] = tmp[str(jj)] / float(M) 
+                        esig_vec[jj] = (esig_vec[jj] + 1.0/M)
                         # process the next pair by kicking out of this loop
                         break
+            tmp['esig'] = esig_vec
+            
+            esig.append(tmp)
         
     return esig
 
@@ -188,6 +189,11 @@ def _makeCoordsList(K):
 
 if __name__=='__main__':
 
+    from copularnd import copularnd
+    from invcopulastat import invcopulastat
+    from scipy.stats import norm
+    from scipy.stats import expon
+
     # some tests on the copula multinomial signature
     tau = 0.4
     K = 4
@@ -195,13 +201,61 @@ if __name__=='__main__':
     # iterate through mnsig to make sure we add upto 1 as a simple sanity check
     val_total = 0
     for ii in range(0,len(mnsig)):
-        val_total = val_total + mnsig[ii]['val']
+        val_total = val_total + mnsig[ii]  #['val']
         
     if(np.isclose(val_total, 1.0)):
         print 'CopulaMNSig total probability check passed!'
     else:
         print 'CopulaMNSig total probability check failed!'
     
-    # TODO:
-    # some tests on the empirical copula multinomial signature calculation
-    # generate a marginal to be normal, and another to be exponential
+    M = 1000
+    
+    ###################### GUMBEL COPULA EXPERIMENT #######################
+    # generate samples of the gumbel copula with tau same as the
+    # empirical signature we calculated above
+    alpha = invcopulastat('Gumbel', 'kendall', tau)
+    U = copularnd('Gumbel', M, alpha)
+    
+    X1 = norm.ppf(U[:,0])       # assume mean=0, var=1
+    X2 = expon.ppf(U[:,1])      # assume mean=0, var=1
+    
+    # combine X and Y into the joint distribution w/ the copula
+    X = np.vstack((X1,X2))
+    X = X.T
+        
+    # calculate the empirical multinomial signature
+    emp_gumbel_mnsig = empirical_copulamnsig(X, K)
+    
+    ###################### FRANK COPULA EXPERIMENT #######################
+    alpha = invcopulastat('Frank', 'kendall', tau)
+    U = copularnd('Frank', M, alpha)
+    
+    X1 = norm.ppf(U[:,0])       # assume mean=0, var=1
+    X2 = expon.ppf(U[:,1])      # assume mean=0, var=1
+    
+    # combine X and Y into the joint distribution w/ the copula
+    X = np.vstack((X1,X2))
+    X = X.T
+        
+    # calculate the empirical multinomial signature
+    emp_frank_mnsig = empirical_copulamnsig(X, K)
+    
+    ###################### CLAYTON COPULA EXPERIMENT #######################
+    alpha = invcopulastat('Clayton', 'kendall', tau)
+    U = copularnd('Clayton', M, alpha)
+    
+    X1 = norm.ppf(U[:,0])       # assume mean=0, var=1
+    X2 = expon.ppf(U[:,1])      # assume mean=0, var=1
+    
+    # combine X and Y into the joint distribution w/ the copula
+    X = np.vstack((X1,X2))
+    X = X.T
+        
+    # calculate the empirical multinomial signature
+    emp_clayton_mnsig = empirical_copulamnsig(X, K)
+    
+    # compare to theoretical multinomial signature
+    print mnsig
+    print emp_gumbel_mnsig[0]['esig']
+    print emp_frank_mnsig[0]['esig']
+    print emp_clayton_mnsig[0]['esig']

copularnd.py
@@ -32,19 +32,19 @@ from scipy.stats import uniform
 copularnd.py contains routines which provide samples of a copula density
 """
 
-def copularnd(family, n, *args):
+def copularnd(family, M, *args):
     """ Generates values of the Gaussian copula
     
     Inputs:
     family -- Should be either 'gaussian', 't', 'clayton', 'frank', or 'gumbel'
-    n      -- the number of samples to generate
+    M      -- the number of samples to generate
     args   -- variable number of arguments depending on which type of copula you are trying to simulate
                 Gaussian -- should be provided a 2x2 rho matrix as a numpy array datatype
                 t        -- should be provided a rho matrix and a nu value
                 Clayton/Frank/Gumbel - should be provided a scalar alpha value
     
     Outputs:
-    U -- a n x 2 matrix of samples from the copula density chosen
+    U -- a M x 2 matrix of samples from the copula density chosen
     """
 
     num_var_args = len(args)
@@ -56,7 +56,7 @@ def copularnd(family, n, *args):
         rho_expected_shape = (2,2)
         if(type(rho)!=np.ndarray or rho.shape!=rho_expected_shape):
             raise ValueError("Gaussian family requires rho to be of type numpy.ndarray with shape=[P x P]")
-        U = _gaussian(n, rho)
+        U = _gaussian(M, rho)
         
     elif(family_lc=='t'):
         return None
@@ -64,23 +64,23 @@ def copularnd(family, n, *args):
         if(num_var_args!=1):
             raise ValueError("Clayton family requires one additional argument -- alpha [scalar]")
         alpha = args[0]
-        U = _clayton(n, alpha)
+        U = _clayton(M, alpha)
     elif(family_lc=='frank'):
         if(num_var_args!=1):
             raise ValueError("Frank family requires one additional argument -- alpha [scalar]")
         alpha = args[0]
-        U = _frank(n, alpha)
+        U = _frank(M, alpha)
     elif(family_lc=='gumbel'):
         if(num_var_args!=1):
             raise ValueError("Gumbel family requires one additional argument -- alpha [scalar]")
         alpha = args[0]
-        U = _gumbel(n, alpha)
+        U = _gumbel(M, alpha)
     else:
         raise ValueError("Unrecognized family of copula")
     
     return U
 
-def _gaussian(n, Rho):
+def _gaussian(M, Rho):
     """
     Generates samples from the Gaussian Copula, w/ dependency
     matrix described by Rho.  Rho should be a numpy square matrix.
@@ -92,9 +92,9 @@ def _gaussian(n, Rho):
     
     # generate samples of the multivariate normal distribution
     # and apply normal cdf to generate U
-    U = np.empty((n,2))
+    U = np.empty((M,2))
     
-    for ii in range(0,n):
+    for ii in range(0,M):
         mvnData = y.rvs()
         U[ii][0] = norm.cdf(mvnData[0])
         U[ii][1] = norm.cdf(mvnData[1])
@@ -112,9 +112,9 @@ def _t():
 # inverting the conditional CDF.
 # This method is outlined in Nelsen's Introduction to Copula's
 
-def _clayton(n, alpha):
-    u1 = uniform.rvs(size=n)
-    p = uniform.rvs(size=n)
+def _clayton(M, alpha):
+    u1 = uniform.rvs(size=M)
+    p = uniform.rvs(size=M)
     if(alpha<np.spacing(1)):
         u2 = p
     else:
@@ -124,9 +124,9 @@ def _clayton(n, alpha):
     
     return U
 
-def _frank(n, alpha):
-    u1 = uniform.rvs(size=n)
-    p = uniform.rvs(size=n)
+def _frank(M, alpha):
+    u1 = uniform.rvs(size=M)
+    p = uniform.rvs(size=M)
     if abs(alpha) > math.log(sys.float_info.max):
         u2 = (u1 < 0).astype(int) + np.sign(alpha)*u1  # u1 or 1-u1
     elif abs(alpha) > math.sqrt(np.spacing(1)):
@@ -137,39 +137,39 @@ def _frank(n, alpha):
     U = np.column_stack((u1,u2))
     return U
 
-def _gumbel(n, alpha):
+def _gumbel(M, alpha):
     if alpha < 1:
         raise ValueError('Bad Gumbel Dependency Parameter!')
     if alpha < (1 + math.sqrt(np.spacing(1))):
-        u1 = uniform.rvs(size=n);
-        u2 = uniform.rvs(size=n);
+        u1 = uniform.rvs(size=M);
+        u2 = uniform.rvs(size=M);
     else:
         # use the Marshal-Olkin method
         # Generate gamma as Stable(1/alpha,1), c.f. Devroye, Thm. IV.6.7
-        u = (uniform.rvs(size=n) - .5) * math.pi # Generate n uniformly distributed RV's between -pi/2 and pi/2
+        u = (uniform.rvs(size=M) - .5) * math.pi # Generate M uniformly distributed RV's between -pi/2 and pi/2
         u2 = u + math.pi/2
-        e  = -1*np.log(uniform.rvs(size=n))
+        e  = -1*np.log(uniform.rvs(size=M))
         t = np.cos(u - u2/alpha)/e
         gamma = np.power(np.sin(u2/alpha)/t,(1.0/alpha)) * t/np.cos(u);
         
         # Frees&Valdez, eqn 3.5
-        u1 = np.exp(-1* (np.power(-1*np.log(uniform.rvs(size=n)), 1.0/alpha) / gamma) )
-        u2 = np.exp(-1* (np.power(-1*np.log(uniform.rvs(size=n)), 1.0/alpha) / gamma) )
+        u1 = np.exp(-1* (np.power(-1*np.log(uniform.rvs(size=M)), 1.0/alpha) / gamma) )
+        u2 = np.exp(-1* (np.power(-1*np.log(uniform.rvs(size=M)), 1.0/alpha) / gamma) )
         
     U = np.column_stack((u1,u2))
     return U
 
 if __name__=='__main__':
     import matplotlib.pyplot as plt
-    n = 1000
+    M = 1000
     rh = 0.6
     Rho = np.array([[1,rh],[rh,1]])
     alpha = 5
     
-    Ug = copularnd('gaussian', n, Rho)
-    Uc  = copularnd('clayton', n,alpha)
-    Uf  = copularnd('frank', n,alpha)
-    Ugu = copularnd('gumbel', n,alpha)
+    Ug = copularnd('gaussian', M, Rho)
+    Uc  = copularnd('clayton', M,alpha)
+    Uf  = copularnd('frank', M,alpha)
+    Ugu = copularnd('gumbel', M,alpha)
     
     plt.scatter(Ug[:,0],Ug[:,1])
     plt.title('Gaussian Copula Samples')